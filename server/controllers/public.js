71dafd9d4f56fa5111e843c46178d8fbda47297f
Nicolas Carlier <n.carlier@nunux.org>
2015-02-17 22:18:38 +0000

    feat: View attachment in the GUI.

--- server/controllers/public.js
+++ server/controllers/public.js
@@ -1,4 +1,5 @@
 var _          = require('underscore'),
+    storage    = require('../storage'),
     errors     = require('../helpers').errors,
     validators = require('../helpers').validators,
     Document   = require('../models').Document;
@@ -38,10 +39,29 @@ module.exports = {
       if (!doc || !_.contains(doc.categories, 'system-public')) {
         return next(new errors.NotFound('Document not found.'));
       }
-      res.writeHead(200, {
-        'Content-Type': doc.contentType
-      });
-      res.end(doc.content);
+      if (!doc.attachment) {
+        res.writeHead(200, {
+          'Content-Type': doc.contentType
+        });
+        res.end(doc.content);
+      } else {
+        var container = storage.getContainerName(req.user.uid, 'documents', doc._id.toString(), 'attachment');
+        return storage.info(container, doc.attachment)
+        .then(function(infos) {
+          if (!infos) {
+            return next(new errors.NotFound('Document attachment not found.'));
+          }
+          // Send the resource file content...
+          res.set('Content-Length', infos.size);
+          res.set('Content-Type', doc.contentType);
+          res.set('Cache-Control', 'public, max-age=86400');
+          res.set('Last-Modified', infos.mtime.toUTCString());
+          return storage.stream(container, doc.attachment)
+          .then(function(s) {
+            s.pipe(res);
+          });
+        });
+      }
     }, next);
   }
 
